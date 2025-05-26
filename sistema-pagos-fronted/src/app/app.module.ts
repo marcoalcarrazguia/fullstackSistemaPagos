c14c0e2604b7fecf2ec6f62e1e09b5c26187c023
marcoalcarraz <marco.alcarraz@mifact.net>
2025-05-26 16:03:39 -0500

    cuarto commit

---
+++ sistema-pagos-fronted/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadEstudiantesComponent } from './load-estudiantes/load-estudiantes.component';
+import { LoadPagosComponent } from './load-pagos/load-pagos.component';
+import { LoginComponent } from './login/login.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoadEstudiantesComponent,
+    LoadPagosComponent,
+    LoginComponent,
+    PagosComponent,
+    EstudiantesComponent,
+    DashboardComponent,
+    LoadPagosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    
+    
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
